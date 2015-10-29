637f9da06b63f40b337d4aa2a4751b3cfc196ad2
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-10-29 18:55:08 +0100

    dodano kod dla Investment service

--- client/app/services/Investment/Investment.service.js
+++ client/app/services/Investment/Investment.service.js
@@ -1,16 +1,103 @@
 'use strict';
 
 angular.module('portalApp')
-  .factory('Investment', function () {
+  .factory('Investment', function ($resource, $q) {
     // Service logic
-    // ...
-
-    var meaningOfLife = 42;
+    var investment = $resource('/api/investment/:id',       // resource url
+        {id: '@id'},                     // default params
+        { update: {method: 'PUT'},        // custom actions
+          getPosts:    {method: 'GET', url: '/api/investment/:id/posts',  isArray: true},
+          getImages:   {method: 'GET', url: '/api/investment/:id/images', isArray: true},
+          findByAdmin: {method: 'GET', url: '/api/investment/findByAdmin/:id', isArray: true},
+          find:        {method: 'POST', url: '/api/investment/find', isArray: true}
+        }
+    );  
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      getAll: function(){
+        var deffered = $q.defer();
+
+        investment.query(function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
+      },
+
+      get: function(id){
+        var deffered = $q.defer();
+
+
+        investment.get({id: id}, function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
+      },
+
+      find: function(searchParams){
+        var deffered = $q.defer();
+
+        investment.find(searchParams, function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
+      },
+
+      findByAdmin: function(id){
+        var deffered = $q.defer();
+
+        investment.findByAdmin({id: id}, function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
+      },
+
+      getPosts: function(id){
+        var deffered = $q.defer();
+
+        investment.getPosts({id: id}, function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
+      },
+
+      getImages: function(id){
+        var deffered = $q.defer();
+
+        investment.getImages({id: id}, function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
+      },
+
+      create: function(obj) {
+          console.log('Im in create');
+          var deffered = $q.defer();
+
+              investment.save(obj, function(data){
+                  
+                  console.log('InvestmentService, create: ' + data);
+
+                  if(data.type == true) {
+                    deffered.resolve(data);
+                  }
+              });
+
+              return deffered.promise;
+      },
+
+      update: function(id, obj) {
+        investment.update({id: id}, obj);
+      },
+
+      delete: function(id) {
+        investment.delete({id: id});
       }
     };
   });
