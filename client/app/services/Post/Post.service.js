2b62f4e97de06ec14e64d780b774a1fff93eba93
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-10-29 19:13:51 +0100

    dodano kod dla Post service

--- client/app/services/Post/Post.service.js
+++ client/app/services/Post/Post.service.js
@@ -1,16 +1,35 @@
 'use strict';
 
 angular.module('portalApp')
-  .factory('Post', function () {
+  .factory('Post', function ($resource, $q) {
     // Service logic
-    // ...
-
-    var meaningOfLife = 42;
+    var post = $resource('/api/post/:id', 
+          {id: '@id'},
+          {update: {method: 'PUT'}
+    });
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+
+      getAll: function(){
+        var deffered = $q.defer();
+
+        post.query(function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;  
+      },
+
+      get: function(id){
+        var deffered = $q.defer();
+
+        post.get({id: id}, function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
       }
     };
+    
   });
