335e73631decb81e7629ae9eb5e757411634c85f
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-10-29 19:51:32 +0100

    dodano kod dla Region service

--- client/app/services/Region/Region.service.js
+++ client/app/services/Region/Region.service.js
@@ -3,14 +3,31 @@
 angular.module('portalApp')
   .factory('Region', function () {
     // Service logic
-    // ...
-
-    var meaningOfLife = 42;
+    var region = $resource('/api/region/:id',
+        {id: '@id'},
+        {
+          getCities: {method: 'GET', url: '/api/region/:id/cities', isArray: true
+        }
+    });
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      getAll: function(){
+        var deffered = $q.defer();
+
+        region.query(function(data){
+          deffered.resolve(data);
+        });
+        return deffered.promise;
+      },
+
+      getCities: function(id){
+        var deffered = $q.defer();
+
+        region.getCities({id: id}, function(data){
+          deffered.resolve(data);
+        });
+        return deffered.promise;
       }
     };
   });
