e43178cd86b7c028c263c45eea7c0412685f8ed1
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-10-29 19:48:25 +0100

    dodano kod dla Follow service

--- client/app/services/Follow/Follow.service.js
+++ client/app/services/Follow/Follow.service.js
@@ -3,14 +3,32 @@
 angular.module('portalApp')
   .factory('Follow', function () {
     // Service logic
-    // ...
-
-    var meaningOfLife = 42;
+    var follow = $resource('/api/follow/:id/', 
+              {id: '@id'}, 
+              {getInvestmentFollowers:     {method: 'GET', url: '/api/follow/investment/:id/', isArray: true},
+               getUserFollowedInvestments: {method: 'GET', url: '/api/follow/user/:id/',       isArray: true}
+    });
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      getInvestmentFollowers: function(id){
+        var deffered = $q.defer();
+
+        follow.getInvestmentFollowers({id: id}, function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
+      },
+
+      getUsersFollowedInvestments: function(id) {
+        var deffered = $q.defer();
+
+        follow.getUserFollowedInvestments({id: id}, function(data){
+          deffered.resolve(data);
+        });
+
+        return deffered.promise;
       }
     };
   });
