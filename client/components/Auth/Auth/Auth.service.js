56cdd21c4daf58d6435f2ac2d049cbddae2417d3
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-10-29 18:50:14 +0100

    dodano kod dla Auth service

--- client/components/Auth/Auth/Auth.service.js
+++ client/components/Auth/Auth/Auth.service.js
@@ -3,14 +3,22 @@
 angular.module('portalApp')
   .factory('Auth', function () {
     // Service logic
-    // ...
-
-    var meaningOfLife = 42;
+    
 
     // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      login: function(data, success, error) {
+        $http.post('/login', data).success(success).error(error);
+      },
+
+      register: function(data, success, error) {
+        $http.post('/register', data).success(success).error(error);
+      },
+
+      logout: function(success, error) {
+        $scope.activeUser = null;
+        delete $localStorage.token;
+        success();
       }
     };
   });
