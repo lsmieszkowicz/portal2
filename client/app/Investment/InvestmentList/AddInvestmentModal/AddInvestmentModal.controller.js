8dca8da7ae8a60e0d2f9496816c746904f2f087a
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-10-29 18:40:09 +0100

    dodany kod AddInvestmentModalu

--- client/app/Investment/InvestmentList/AddInvestmentModal/AddInvestmentModal.controller.js
+++ client/app/Investment/InvestmentList/AddInvestmentModal/AddInvestmentModal.controller.js
@@ -1,6 +1,29 @@
 'use strict';
 
 angular.module('portalApp')
-  .controller('AddInvestmentModalCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('AddInvestmentModalCtrl', function ($scope, $modalInstance, Investment, $location) {
+		
+	$scope.add = function(){
+		console.log('New investment data: ' + $scope.newInvestment + 'activeUser id: ' + $scope.$parent.activeUser.id);
+		var investmentToCreate = {
+			name:         $scope.newInvestment.name,
+			description:  $scope.newInvestment.description,
+			city:         1,
+			admin: 		  $scope.$parent.activeUser.id,
+			creationDate: new Date()
+		}
+
+		Investment.create(investmentToCreate)
+        .then(function(result){
+        	console.log(result);
+            console.log('inwestycja utworzona');
+            $modalInstance.close();
+            $location.path('/investment/show/' + result.data.insertId);
+         });
+	};
+
+	$scope.cancel = function(){
+		$modalInstance.dismiss('canceled');
+	};
+		  
   });
