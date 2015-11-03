c79f65c56506dc22f6821531fbf6d82b18ab6291
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-11-03 22:08:17 +0100

    szkielet api dla inwestycji

--- server/api/investment/index.js
+++ server/api/investment/index.js
@@ -5,6 +5,13 @@ var controller = require('./investment.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-
+router.get('/api/investment',                 controller.getAll);
+router.get('/api/investment/:id',             controller.get);
+router.get('/api/investment/:id/posts',       controller.getPosts);
+router.get('/api/investment/:id/images',      controller.getImages);
+router.get('/api/investment/findByAdmin/:id', controller.findByAdmin);
+router.post('/api/investment/find',			  controller.find);
+router.post('/api/investment',			      controller.create);
+router.put('/api/investment/:id',			  controller.update);
+router.delete('/api/investment/:id',		  controller.remove);
 module.exports = router;
--- server/api/investment/investment.controller.js
+++ server/api/investment/investment.controller.js
@@ -6,3 +6,38 @@ var _ = require('lodash');
 exports.index = function(req, res) {
   res.json([]);
 };
+
+exports.getAll = function(req, res) {
+	res.json([]);
+};
+
+exports.get = function(req, res) {
+	res.json([]);
+};
+
+exports.getPosts = function(req, res) {
+	res.json([]);
+};
+
+exports.getImages = function(req, res) {
+	res.json([]);
+};
+
+exports.findByAdmin = function(req, res) {
+	res.json([]);
+};
+
+exports.find = function(req, res) {
+	res.json([]);
+};
+
+exports.create = function(req, res) {
+	res.json([]);
+};
+
+exports.update = function(req, res) {
+	res.json([]);
+};
+exports.remove = function(req, res) {
+	res.json([]);
+};
