17f43796922e13d5236892e4b4639a9a2b0c0b7d
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-11-09 19:20:09 +0100

    pelne api dla miast

--- server/api/city/city.controller.js
+++ server/api/city/city.controller.js
@@ -1,8 +1,97 @@
 'use strict';
 
 var _ = require('lodash');
+var City = require('./city.model');
 
-// Get list of citys
-exports.index = function(req, res) {
-  res.json([]);
+
+exports.get = function(req, res) {
+	var id = req.params.id;
+	
+	City.get(id, function(err, data){
+		if(err) {
+			res.json({
+				status: 'error',
+				error: err
+			});
+		}
+		else {
+			res.json({
+				status: 'ok',
+				data: data
+			});	
+		}
+	});
+};
+
+exports.getAll = function(req, res) {
+	City.getAll(function(err, data){
+		if(err) {
+			res.json({
+				status: 'error',
+				error: err
+			});
+		}
+		else {
+			res.json({
+				status: 'ok',
+				data: data
+			});	
+		}
+	});  
+};
+
+exports.create = function(req, res) {
+	var postData = req.body;
+	
+	City.create(postData, function(err){
+		if(err) {
+			res.json({
+				status: 'error',
+				error: err
+			});
+		}
+		else {
+			res.json({
+				status: 'ok',
+			});	
+		}
+	});  
+};
+
+exports.update = function(req, res) {
+	var id = req.params.id;  
+	var putData = req.body;
+	
+	City.update(id, putData, function(err){
+		if(err) {
+			res.json({
+				status: 'error',
+				error: err
+			});
+		}
+		else {
+			res.json({
+				status: 'ok',
+			});	
+		}
+	});
+};
+
+exports.remove = function(req, res) {
+	var id = req.params.id;  
+
+	City.remove(id, function(err){
+		if(err) {
+			res.json({
+				status: 'error',
+				error: err
+			});
+		}
+		else {
+			res.json({
+				status: 'ok',
+				data: data
+			});	
+		}
+	});
 };
---
+++ server/api/city/city.model.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var db = require('../../database');
+var connection = db.connection;
+
+
+module.exports = {
+	get: function(id, callback){
+		connection.query("SELECT * FROM city WHERE id = ?", id, function(err, rows, fields) {
+			if(err) throw err;
+
+			callback(err, rows[0]);
+		});
+	},
+
+	getAll: function(callback){
+		connection.query("SELECT * FROM city", [], function(err, rows, fields) {
+			if(err) throw err;
+		
+			callback(err, rows);
+		});
+	},
+
+	create: function(newCity, callback){
+		connection.query("INSERT INTO city (name, region, admin_id) VALUES (?, ?, ?)", [newCity.name, newCity.region, newCity.admin_id], function(err, rows, fields){
+			if(err) throw err;
+
+			callback(err, rows);
+		});
+	},
+
+	update: function(id, newData, callback){
+		connection.query("UPDATE city SET name = ?, region = ?, admin_id = ? WHERE id = ?", [newData.name, newData.region, newData.admin_id, id], function(err, rows, fields){
+			if(err) throw err;
+
+			callback(err, rows);
+		});
+	},
+
+	remove: function(id, callback){
+		connection.query("DELETE FROM city where id = ?", id, function(err, rows, fields){
+			if(err) throw err;
+
+			callback(err);
+		});		
+	}
+};
