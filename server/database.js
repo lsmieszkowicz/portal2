1eef3fea0451408c5be8e94f9446ec9e9bbb98c6
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-11-21 18:20:13 +0100

    inicjalizacja i czyszczenie testowej bazy danych po wykonaniu testow

--- server/database.js
+++ server/database.js
@@ -10,5 +10,24 @@ var config = require('./config/environment');
 var connection = mysql.createConnection(config.sql);
 
 module.exports = {
-	connection: connection
+	
+	connection: connection,
+
+	initTableForTest: function(tableName, obj, callback){
+		
+		connection.query("INSERT INTO ?? SET ?", [tableName, obj], function(err, result){
+			if(err) return err;
+
+			callback();
+		});
+	},
+
+	cleanTableForTest: function(tableName, callback){
+		
+		var query = connection.query("DELETE FROM ?? WHERE id > 1", [tableName], function(err, result){
+			if(err) return err;
+			
+			callback();
+		});
+	}
 }
