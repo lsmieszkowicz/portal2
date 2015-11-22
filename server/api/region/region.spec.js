91612465586c0d6598dfccb7151ee39ca389bd36
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-11-22 09:47:09 +0100

    testy regionow

--- server/api/region/region.spec.js
+++ server/api/region/region.spec.js
@@ -3,18 +3,39 @@
 var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
+var db = require('../../database');
 
-// describe('GET /api/regions', function() {
+describe('Post API test', function(){
 
-//   it('should respond with JSON array', function(done) {
-//     request(app)
-//       .get('/api/regions')
-//       .expect(200)
-//       .expect('Content-Type', /json/)
-//       .end(function(err, res) {
-//         if (err) return done(err);
-//         res.body.should.be.instanceof(Array);
-//         done();
-//       });
-//   });
-// });
+	it('should return all regions', function(done){
+		request(app)
+			.get('/api/regions')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res){
+				if(err) return done(err);
+
+				res.body.should.be.instanceof(Object);
+				res.body.status.should.equal("ok");
+				res.body.data.should.be.instanceof(Array);
+				res.body.data.length.should.equal(16);
+				done();
+			});
+	});
+
+	it("should return all region's cities" , function(done){
+		request(app)
+			.get('/api/regions/1/cities')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res){
+				if(err) return done(err);
+
+				res.body.should.be.instanceof(Object);
+				res.body.status.should.equal("ok");
+				res.body.data.should.be.instanceof(Array);
+				res.body.data.length.should.equal(1);
+				done();
+			});
+	});
+}); 
