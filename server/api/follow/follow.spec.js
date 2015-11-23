d710ff3dc82fec1d190a8e96fcef2ae4e837f680
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-11-23 12:41:35 +0100

    testy api follow

--- server/api/follow/follow.spec.js
+++ server/api/follow/follow.spec.js
@@ -4,17 +4,63 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-// describe('GET /api/follow', function() {
+var db = require('../../database');
 
-//   it('should respond with JSON array', function(done) {
-//     request(app)
-//       .get('/api/follow')
-//       .expect(200)
-//       .expect('Content-Type', /json/)
-//       .end(function(err, res) {
-//         if (err) return done(err);
-//         res.body.should.be.instanceof(Array);
-//         done();
-//       });
-//   });
-// });
+describe('Follow API test', function(){
+
+	var newFollow = {
+		id: 10,
+		investment_id: 4,
+		user_id: 4
+	};
+
+	it('should return all followers of investment with id 1', function(done){
+		request(app)
+			.get('/api/follow/investment/1')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res){
+				if(err) return done(err);
+
+				res.body.should.be.instanceof(Object);
+				res.body.status.should.equal('ok');
+
+				res.body.data.should.be.instanceof(Array);
+				res.body.data.length.should.equal(3);	
+				done();
+			})
+	})
+
+	it('should return all investments followerd by user with id 1', function(done){
+		request(app)
+			.get('/api/follow/user/1')
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res){
+				if(err) return done(err);
+
+				res.body.should.be.instanceof(Object);
+				res.body.status.should.equal('ok');
+
+				res.body.data.should.be.instanceof(Array);
+				res.body.data.length.should.equal(2);	
+				done();
+			})
+	})
+
+	it('should create new follow relation', function(done){
+		request(app)
+			.post('/api/follow')
+			.send(newFollow)
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end(function(err, res){
+				if(err) return done(err);
+
+				res.body.should.be.instanceof(Object);
+				res.body.status.should.equal('ok');
+				done();
+			})
+	})
+
+});
