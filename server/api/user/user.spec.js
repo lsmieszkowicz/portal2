2b671cae8e9b49ccb2db48c19d3892f0c799d7af
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-11-23 22:42:06 +0100

    testy api uzytkownikow

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -4,17 +4,72 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-// describe('GET /api/users', function() {
+describe('User API test', function(){
+	
+	var userUpdate = {
+		login: 'newAdmin',
+		name: 'newAdminName',
+		surname: 'newAdminSurname',
+		email: 'newEmail@domain.com',
+		password: 'newpass'
+	};
 
-//   it('should respond with JSON array', function(done) {
-//     request(app)
-//       .get('/api/users')
-//       .expect(200)
-//       .expect('Content-Type', /json/)
-//       .end(function(err, res) {
-//         if (err) return done(err);
-//         res.body.should.be.instanceof(Array);
-//         done();
-//       });
-//   });
-// });
+	it('should return all users', function(done){
+		request(app)
+		.get('/api/users')
+		.expect(200)
+		.expect('Content-Type', /json/)
+		.end(function(err, res){
+			if(err) return done(err);
+			res.body.should.be.instanceof(Object);
+			res.body.status.should.equal('ok');
+			res.body.data.should.be.instanceof(Array);
+			res.body.data.length.should.equal(2);
+			done();
+		});
+	});
+
+	it('should return user with id: 1', function(done){
+		request(app)
+		.get('/api/users/1')
+		.expect(200)
+		.expect('Content-Type', /json/)
+		.end(function(err, res){
+			if(err) return done(err);
+
+			res.body.data.id.should.equal(1);
+			res.body.data.login.should.equal('test');
+			res.body.data.name.should.equal('test_name');
+			res.body.data.surname.should.equal('test_surname');
+			res.body.data.email.should.equal('login@domain.com');
+			
+			done();
+		});
+	});
+
+	it('should update user with id: 2', function(done){
+		request(app)
+		.put('/api/users/2')
+		.send(userUpdate)
+		.expect(200)
+		.expect('Content-Type', /json/)
+		.end(function(err, res){
+			if(err) return done(err);
+
+			done();
+		});
+	});
+
+	// it('should delete user with id 2', function(done){
+	// 	request(app)
+	// 	.delete('/api/users/2')
+	// 	.expect(200)
+	// 	.expect('Content-Type', /json/)
+	// 	.end(function(err, res){
+	// 		if(err) return done(err);
+
+	// 		done();
+	// 	});
+	// });
+
+});
