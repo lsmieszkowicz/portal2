c5ff373cb999a801ba0a566e58f95b5221cc8e18
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-11-25 21:39:21 +0100

    test logowania

--- server/api/auth/auth.spec.js
+++ server/api/auth/auth.spec.js
@@ -4,17 +4,31 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/auth', function() {
+var jwt = require('jsonwebtoken');
 
-  it('should respond with JSON array', function(done) {
+describe('Auth tests', function(){
+
+  var loginData = {
+      login: 'test',
+      password: 'password'
+  };
+
+  it('should login and return token and user data', function(done){
     request(app)
-      .get('/api/auth')
+      .post('/api/auth/login')
+      .send(loginData)
       .expect(200)
       .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
+      .end(function(err, res){
+        if(err) return done(err);
+
+        var token = res.body.data.token;
+
+        jwt.verify(token, process.env.SESSION_SECRET);
+
         done();
       });
+
   });
+
 });
