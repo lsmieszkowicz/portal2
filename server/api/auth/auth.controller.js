967205acff6c31b399bb322aa591fb6ec960dc72
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-11-25 20:47:04 +0100

    dodano mozliwosc logowania

--- server/api/auth/auth.controller.js
+++ server/api/auth/auth.controller.js
@@ -7,8 +7,31 @@ exports.login = function(req, res){
 	
 	var loginData = req.body;
 
-	authModel.login(loginData, function(){
-
+	authModel.login(loginData, function(err, user){
+		if(err) {
+			res.json({
+				status: "error",
+				error: err
+			});
+		}
+		else {
+			if(user){
+				var token = jwt.sign(user, process.env.SESSION_SECRET);
+				
+				res.json({
+					status: 'ok',
+					data: {
+						user: user,
+						token: token
+					}
+				});
+			}
+			else{
+				res.json({
+					status: 'nouser',
+				});
+			}
+		}
 	});
 
 };
--- server/api/auth/auth.model.js
+++ server/api/auth/auth.model.js
@@ -9,7 +9,13 @@ module.exports = {
 		var login = loginData.login;
 		var password = loginData.password;
 
-		
+		var sql = 'SELECT id, login, name, surname, email FROM user WHERE login = ? AND PASSWORD = ?';
+
+		connection.query(sql, [login, password], function(err, rows, fields) {
+			if(err) throw err;
+
+			calback(err, rows[0]);
+		});
 	},
 
 
