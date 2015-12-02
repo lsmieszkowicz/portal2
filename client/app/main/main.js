6cd57d65a1f4dcb445598974355d85b6f4f75939
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2015-12-02 22:23:52 +0100

    zmiana sciezki do nieuzywanego widoku

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -3,7 +3,7 @@
 angular.module('portalApp')
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/kjshdfkjsdhfkjsdhfkjshdf', {
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
