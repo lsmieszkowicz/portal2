aa9a5ee6db01d73ce10b1a91b8c01346b8e3c284
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2016-01-16 15:41:26 +0100

    wyswietlanie mapy bez markerow w widoku miasta

--- client/app/City/CityShow/CityShow.controller.js
+++ client/app/City/CityShow/CityShow.controller.js
@@ -1,17 +1,38 @@
 'use strict';
 
 angular.module('portalApp')
-  .controller('CityShowCtrl', function ($scope, $routeParams, $q, City, Region) {
+  .controller('CityShowCtrl', function ($scope, $routeParams, $q, City, Region, uiGmapGoogleMapApi) {
     
     $scope.currentId = $routeParams.id;
     $scope.city = {};
     $scope.investments = [];
+    $scope.map = {
+    	center: {
+  			latitude: 52.03,
+  			longitude: 19.27
+  		},
+  		zoom: 6
+  	};
 
     var cityPromise = City.get($scope.currentId);
 
     cityPromise
     	.then(function(result){
     		$scope.city = result.data;
+
+    		// inicjalizacja miasta na mapie
+    		geocodeAddress($scope.city.name, function(latLng){
+	            
+		        $scope.$apply(function() { 
+  	    			$scope.map = {
+  	    				center: {
+  	    					latitude: latLng.lat(),
+  	    					longitude: latLng.lng()
+  	    				},
+  	    				zoom: 13
+  	    			}
+            });
+        });
     	});
 
     cityPromise
@@ -21,11 +42,17 @@ angular.module('portalApp')
     				$scope.investments = result.data;
     			});
 
-
     	});
 
-    cityPromise
-    	.then()
+    
+    var geocodeAddress = function(address, callback){
+    	var geocoder = new google.maps.Geocoder();
 
+    	geocoder.geocode({address: address}, function(results, status){
+    		if(status == google.maps.GeocoderStatus.OK){
+    			callback(results[0].geometry.location);
+    		}
+    	});	
+    };
   
   });
