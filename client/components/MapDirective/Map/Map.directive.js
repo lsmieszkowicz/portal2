680a13381756e0edf5c3b03765e81bfe26b489f1
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2016-04-20 22:11:52 +0200

    wyswietlanie mapy i mozliwosc rysowania po niej w dyrektywie Map

--- client/components/MapDirective/Map/Map.directive.js
+++ client/components/MapDirective/Map/Map.directive.js
@@ -1,11 +1,54 @@
 'use strict';
 
 angular.module('portalApp')
-  .directive('Map', function () {
+  .directive('map', function () {
     return {
+      
       templateUrl: 'components/MapDirective/Map/Map.html',
+    
       restrict: 'EA',
-      link: function (scope, element, attrs) {
+
+	  scope: {
+		  investments: '=',
+		  cityFocus: '=',
+		  editable: '='
+	  },
+	  
+	  controller: function($scope, uiGmapGoogleMapApi){
+		  
+		  $scope.zoom = 6;
+		  $scope.center = {
+  			latitude: 52.03,
+  			longitude: 19.27
+		  };
+
+		  $scope.drawingManagerOptions = {
+
+				drawingMode: google.maps.drawing.OverlayType.null,
+				drawingControl: $scope.editable || false,
+				drawingControlOptions: {
+					position: google.maps.ControlPosition.TOP_CENTER,
+					drawingModes: []
+				}
+		  };
+		  
+		  if($scope.editable === true){
+			  $scope.drawingManagerOptions.drawingControlOptions.drawingModes = [
+			  	   google.maps.drawing.OverlayType.MARKER,
+			  	   google.maps.drawing.OverlayType.POLYLINE,
+			  	   google.maps.drawing.OverlayType.CIRCLE,
+			  	   google.maps.drawing.OverlayType.RECTANGLE
+			  ]
+		  }
+
+		  $scope.drawingManagerControl = {
+
+		  };
+	  },
+
+	  link: function (scope, element, attrs) {
+	
       }
-    };
+   
+   	};
   });
