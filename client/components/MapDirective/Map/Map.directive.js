68d67b6603fa48a27f77b9755d4ae8f50c23a7d5
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2016-04-21 19:44:34 +0200

    dyrektywa map - scope, eventy dla markerow, plyline i prototyp dla rectangle

--- client/components/MapDirective/Map/Map.directive.js
+++ client/components/MapDirective/Map/Map.directive.js
@@ -22,6 +22,11 @@ angular.module('portalApp')
   			longitude: 19.27
 		  };
 
+		  $scope.markers = [];
+		  $scope.polylines = [];
+		  $scope.rectangles = [];
+		  $scope.circles = [];
+
 		  $scope.drawingManagerOptions = {
 
 				drawingMode: google.maps.drawing.OverlayType.null,
@@ -44,6 +49,54 @@ angular.module('portalApp')
 		  $scope.drawingManagerControl = {
 
 		  };
+
+
+		  $scope.drawingManagerEvents = {
+			 
+			 markercomplete: function(dm, name, scope, objs){
+				 var marker = {
+					 latitude: objs[0].getPosition().lat(),
+					 longitude: objs[0].getPosition().lng()
+				 };
+				 $scope.markers.push(marker);
+		     },
+			 
+			 polylinecomplete: function(dm, name, scope, objs){
+			 	 var path = objs[0].getPath().getArray();
+				 //console.log(path[0]);
+				 for(var i in path){
+					 var pathPoint = {
+						 latitude: path[i].lat(),
+						 longitude: path[i].lng()
+					 };
+					 console.log(pathPoint);
+				 }
+			 },
+
+			 rectanglecomplete: function(dm, name, scope, objs){
+				 
+				 var ne = {
+					 latitude: objs[0].getBounds().getNorthEast().lat(),
+					 longitude: objs[0].getBounds().getNorthEast().lng(),
+				 };
+			     
+			     var sw = {
+					 latitude: objs[0].getBounds().getSouthWest().lat(),
+					 longitude: objs[0].getBounds().getSouthWest().lng(),
+				 };
+				
+				 var rect = {
+					 northEast: ne,
+					 southWest: sw
+				 };
+
+				 $scope.rectangles.push(rect);
+			 },
+
+			 circlecomplete: function(dm, name, scope, objs){
+			 	 console.log(objs[0]);
+			 }
+	  	  };
 	  },
 
 	  link: function (scope, element, attrs) {
