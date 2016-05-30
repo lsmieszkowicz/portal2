42fad72c408e7cc88a8b9b4796850fa30ceb02a0
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2016-05-30 21:52:25 +0200

    ocenianie uzytkownika z widoku uzytkownika

--- client/app/User/User.controller.js
+++ client/app/User/User.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('portalApp')
-  .controller('UserCtrl', function ($scope, $modal) {
+  .controller('UserCtrl', function ($scope, $modal, $localStorage, User) {
     
+  	$scope.userRankButtons = {};
+
     $scope.editUser = function(){
     	$modal.open({
     		templateUrl: 'app/User/EditUserModal/EditUserModal.html',
@@ -21,5 +23,78 @@ angular.module('portalApp')
     	});
     }
 
+	$scope.incrementRank = function(){
+		if($scope.showingUser.id === $scope.activeUser.id){
+			alert("Nie możesz ocenić sam siebie");
+		}
+		else{
+			User.updateRank($scope.showingUser.id, {rank: 1})
+			.then(function(){
+				$scope.showingUser.rank++;
+				
+				// anulowanie negatywnego glosu
+				if($scope.userRankButtons.minusLocked === true){
+					$scope.userRankButtons.plusLocked = false;
+					$scope.userRankButtons.minusLocked = false;
+				}
+				// glos negatywny
+				else{
+					$scope.userRankButtons.plusLocked = true;
+					$scope.userRankButtons.minusLocked = false;
+				}
+				
+				$localStorage.userRankButtons[$scope.showingUser.id] = $scope.userRankButtons;
+			});
+		}
+
+	};
+
+	$scope.decrementRank = function(){
+		if($scope.showingUser.id === $scope.activeUser.id){
+			alert("Nie możesz ocenić sam siebie");
+		}
+		else{
+			User.updateRank($scope.showingUser.id, {rank: -1})
+			.then(function(){
+				$scope.showingUser.rank--;
+
+				// anulowanie pozytywnego glosu
+				if($scope.userRankButtons.plusLocked === true){
+					$scope.userRankButtons.plusLocked = false;
+					$scope.userRankButtons.minusLocked = false;
+				}
+				// glos negatywny
+				else {
+					$scope.userRankButtons.plusLocked = false;
+					$scope.userRankButtons.minusLocked = true;
+				}
+
+				$localStorage.userRankButtons[$scope.showingUser.id] = $scope.userRankButtons;
+			});			
+		}
+
+	};
+
+	var initRankButtons = function(){
+		if(typeof $scope.showingUser !== 'undefined'){
+			console.log($scope.showingUser);
+			if(typeof $localStorage.userRankButtons !== 'undefined'){
+
+				if(typeof $localStorage.userRankButtons[$scope.showingUser.id] !== 'undefined'){
+					$scope.userRankButtons = $localStorage.userRankButtons[$scope.showingUser.id];
+				}
+			} 
+			else {
+				$localStorage.userRankButtons = {};
+				$scope.userRankButtons = {
+					plusLocked: false,
+					minusLocked: false
+				}
+			}
+		}
+			
+	};
+
+	initRankButtons();
 
   });
